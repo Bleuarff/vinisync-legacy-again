70ff16fd54541e6ea3d7a022aaa66dfe08f17ac8
bleuarff <bleuarff@gmail.com>
2017-09-21 22:12:08 +0200

    Complete normalizer + tests

--- services/normalizer.js
+++ services/normalizer.js
@@ -3,14 +3,16 @@
 const Undiacritics = require('../utils/undiacritics.js')
 
 // stop words to not capitalize, except at the beginning of string
-const stopWords = 'a|à|c|d|j|l|m|n|s|t|y|au|ça|ce|ces|ci|des|du|dos|en|et|hui|ici|la|le|les|là|ma|mes|mon|ni|nous|or|ou|où|par|pas|peu|pour|que|quel|quelle|quelles|quels|qui|sa|sans|ses|si|sien|son|sont|sous|sur|ta|tel|tels|tes|ton|tous|tout|trop|très|tu|votre|vous|vu'.split('|'),
+const stopWords = 'a|à|c|d|j|l|m|n|s|t|y|au|aux|ça|ce|ces|ci|de|des|du|dos|en|et|hui|ici|la|le|les|là|ma|mes|mon|ni|nous|or|ou|où|par|pas|peu|pour|que|quel|quelle|quelles|quels|qui|sa|sans|ses|si|sien|son|sont|sous|sur|ta|tel|tels|tes|ton|tous|tout|trop|très|tu|votre|vous|vu'.split('|'),
       undiacritics = new Undiacritics(),
       // list string or patterns to replace, with substitutions
       patterns = [
         ['Chateau', 'Château'],
         [/\bSt(e?)\b/g, 'Saint$1'], // expand st, ste into saint, sainte
         [/\b(Sainte?)\s+\b/g, '$1-'] // hyphenate Saint(e) when followed by a word
-      ]
+      ],
+      // split an input by words
+      wordsRx = /\b[\wàäâéèêëìïîôöòüûùç]+\b/gi // TODO: handle more accented chars
 
 class Normalizer{
   // get standard, lowercase, non-accented string, with non-alphanumeric chars converted to space
@@ -21,24 +23,26 @@ class Normalizer{
     return value
   }
 
-  // # Applies each pattern on input and replaces it with corresponding substitution
-  // @_replacePatterns: (input) ->
-  //   for item in Normalizer.patterns
-  //     input = input.replace item[0], item[1]
-  //   return input
-  //
-  //
-  // # Converts input to title case, except stop words not at the beginning
-  // @_toTitleCase: (input) ->
-  //   # TODO: handle more accented chars
-  //   rx = /\b[\wàäâéèêëìïîôöòüûùç]+\b/gi
-  //   return input.replace rx, (match, offset) ->
-  //     if (Normalizer.stopWords.indexOf(match) > -1 && offset  > 0)
-  //       rep = match
-  //     else
-  //       rep = match[0].toUpperCase() + match.substring(1).toLowerCase()
-  //
-  //     return rep
+  // # Applies the ccorrect pattern on input and replaces it with corresponding substitution
+  static _replacePatterns(input){
+    patterns.forEach(([pattern, fixValue]) => {
+      input = input.replace(pattern, fixValue)
+    })
+    return input
+  }
+
+
+  // Converts input to title case, except stop words not at the beginning
+  static _toTitleCase(input){
+    return input.replace(wordsRx, (match, offset) => {
+      var rep
+      if (stopWords.indexOf(match) > -1 && offset > 0)
+        rep = match
+      else
+        rep = match[0].toUpperCase() + match.substring(1).toLowerCase()
+      return rep
+    })
+  }
 }
 
 module.exports = exports = Normalizer
--- test/normalizer.js
+++ test/normalizer.js
@@ -6,13 +6,43 @@ const sut = require('../services/normalizer.js')
 describe('Normalizer', () => {
   describe('standard form', () => {
     var testData = [
-      ['Château', 'chateau'], ['St Emilion', 'st emilion']
+      ['Château', 'chateau'], ['St Emilion', 'st emilion'], ['PêraY', 'peray']
     ]
 
-    testData.forEach(pair => {
-      let [input, expected] = pair
+    testData.forEach(([input, expected]) => {
       it(expected, () => {
         var output = sut.getStandardForm(input)
+        assert.equal(output, expected)
+      })
+    })
+  })
+
+  describe('replace patterns', () => {
+    var testData = [
+      ['Chateau St Ange', 'Château Saint-Ange'],
+      ['Saint Emilion', 'Saint-Emilion'],
+      ['Ste Joseph', 'Sainte-Joseph']
+    ]
+
+    testData.forEach(([input, expected]) => {
+      it(expected, () => {
+        var output = sut._replacePatterns(input)
+        assert.equal(output, expected)
+      })
+    })
+  })
+
+  describe('title case', () => {
+    var testData = [
+      ['chateau', 'Chateau'],
+      ['la pêche aux moules', 'La Pêche aux Moules'],
+      ['aujourd\'hui en france', 'Aujourd\'hui en France'],
+      ['les pierres sèches de simon la pallu', 'Les Pierres Sèches de Simon la Pallu']
+    ]
+
+    testData.forEach(([input, expected]) => {
+      it(expected, () => {
+        var output = sut._toTitleCase(input)
         assert.equal(output, expected)
       })
     })
