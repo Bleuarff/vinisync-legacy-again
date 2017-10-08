059a99f790cb98698fc231249050290716151abb
bleuarff <bleuarff@gmail.com>
2017-10-08 21:45:27 +0200

    start client side. App elem, stubs for home & 404

---
+++ assets/elements/vni-app/vni-app.js
@@ -0,0 +1,40 @@
+'use strict'
+
+class App extends Polymer.Element {
+  static get is(){return 'vni-app'}
+  static get properties(){
+    return {
+      page: {
+        type: String,
+        observer: '_pageChanged'
+      }
+    }
+  }
+
+  static get observers(){
+    return [
+      '_routePageChanged(routeData.page)'
+    ]
+  }
+
+  ready(){
+    super.ready()
+    console.log('hello app')
+  }
+
+  _routePageChanged(page){
+    if (page === this.page || typeof page === 'undefined')
+      return
+
+    this.page = page || 'home'
+  }
+
+  _pageChanged(page){
+    var resolvedPageUrl = this.resolveUrl(`../vni-${page}/vni-${page}.html`)
+    Polymer.importHref(resolvedPageUrl, null, () => {
+      this.page = 'z404'
+    }, true)
+  }
+}
+
+window.customElements.define(App.is, App)
---
+++ assets/elements/vni-home/vni-home.js
@@ -0,0 +1,5 @@
+'use strict'
+
+class Home extends Polymer.Element{
+  static get is(){ return 'vni-home'}
+}
