699147ddad4df153a9239c297652f368dfadcc74
bleuarff <bleuarff@gmail.com>
2017-10-26 00:08:12 +0200

    entry retrieval ok

--- assets/elements/vni-entry/vni-entry.js
+++ assets/elements/vni-entry/vni-entry.js
@@ -4,7 +4,25 @@ class Entry extends BaseElement{
   static get is(){return 'vni-entry'}
   static get properties(){
     return {
-
+      entry: Object,
+      edit: Boolean,
+      countries: {
+        type: Array,
+        value: ['Afrique du sud', 'Allemagne', 'Argentine', 'Australie',
+          'Chili', 'Espagne', 'Etats-Unis', 'France', 'Italie', 'Nouvelle Zélande',
+          'Portugal'],
+        readonly: true
+      },
+      containings: {
+        type: Array,
+        value: ['37.5cl', '50cl', '75cl', '1.5L', '3l', '4.5l','6l'],
+        readonly: true
+      },
+      currentYear: {
+        type: Number,
+        value: (new Date()).getUTCFullYear(),
+        readonly: true
+      }
     }
   }
 
@@ -14,17 +32,28 @@ class Entry extends BaseElement{
   }
 
   show(e){
-    console.log('show !')
+    // console.log(`show ! path: ${this.route.path}`)
     if (!window.user){
       console.log('entry: not logged')
       return
     }
 
-    this._getEntry()
+    if (this.route.path)
+      this._getEntry(this.routeData.id)
+    else
+      console.log('new entry')
   }
 
-  _getEntry(){
-    console.log('TODO: get entry')
+  async _getEntry(id){
+    // console.log(`TODO: get entry ${id}`)
+    try{
+      let entry = await this.send(`/api/entry/${id}`)
+      this.entry = entry
+    }
+    catch(err){
+      console.error(err)
+      this.dispatchEvent(new CustomEvent('error', {detail: 'Echec récup entrée', bubbles: true, composed: true}))
+    }
   }
 }
 
