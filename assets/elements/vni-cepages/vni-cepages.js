f0028c6240a0d4d0096da88435ae788cb0358d61
bleuarff <bleuarff@gmail.com>
2017-11-01 15:42:27 +0100

    cepages: add new cepage to list

--- assets/elements/vni-cepages/vni-cepages.js
+++ assets/elements/vni-cepages/vni-cepages.js
@@ -30,6 +30,18 @@ class Cepages extends BaseElement{
     var node = e.currentTarget.parentElement
     setTimeout(() => node.classList.remove('selected'), 1)
   }
+
+  // add cepage to list
+  addCepage(e){
+    var value = this.$.newCepage.value
+
+    // check value is not empty & not already in list
+    if (value && this.cepages.map(x => x.toLowerCase()).indexOf(value.toLowerCase()) === -1){
+      this.push('cepages', this.$.newCepage.value)
+      this.$.newCepage.value = ''
+    }
+    this.$.newCepage.focus()
+  }
 }
 
 window.customElements.define(Cepages.is, Cepages)
